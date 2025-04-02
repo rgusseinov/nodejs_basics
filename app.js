8a799bc9572ebb96af5243a3567404abbe61e7e9
Ruslan Gusseinov <r.gusseinov@mail.ru>
2025-04-02 10:29:35 +0500

    start

---
+++ app.js
@@ -0,0 +1,38 @@
+// console.log(process.pid);
+
+
+/* const dotenv = require('dotenv');
+dotenv.config();
+console.log(process.env.AWS_IP); */
+
+/* 
+  pass arguments in script. 
+  node app.js a b c 
+*/
+// console.log(process.argv);
+
+
+const path = require('path');
+console.log(path.join(__dirname, 'root', 'app', 'www'));
+console.log(path.join(__dirname, 'root', '..', '..'));
+
+const fullpath = path.resolve(__dirname, 'first', 'second.js');
+
+console.log(path.parse(fullpath));
+
+/* {
+  root: '/',
+  dir: '/home/ruslanglk/nodejs_basics/first',
+  base: 'second',
+  ext: '',
+  name: 'second'
+} */
+
+/* console.log('Название файла', path.basename(fullpath));
+console.log('Расширение', path.extname(fullpath)); */
+
+const siteURL = `http://localhost:8081/users?id=35&type=one`;
+
+const url = new URL(siteURL);
+
+console.log(url);
